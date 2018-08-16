94a80f4213cfde679266ffbe62f6d2367778f7c9
Bhakti Sawarkar <bhakti123.java@gmail.com>
2018-08-16 01:10:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+var katzDeli = [];
+
+function takeANumber(katzDeliLine, customerName)
+{
+  katzDeliLine.push(customerName);
+  return "welcome,"+ customerName +". You are number" + (katzDeliLine.length)+ "in line."
+}
+
+function nowServing(katzDeliLine)
+{
+  if(katzDeliLine.length == 0){
+    return "There is nobody waiting to be served!"
+  }
+  var current = katzDeliLine.shift()
+  return "currently serving" + current + ".";
+}
+
+function currentLine(katzDeliLine)
+{
+  var current = [];
+
+  let i = 0;
+  while(i < katzDeliLine.length)
+{
+  current.push(""+(i + 1)+ "." + katzDeliLine[i])
+  i++;
+}
+}
+
+if (katzDeliLine == 0)
+{
+  return "The line is currently empty"
+}
+else {
+  return "The line is currently:" + current;
+}
+}
